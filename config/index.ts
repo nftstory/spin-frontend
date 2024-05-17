3f2e47241f9f7b298fc7e81c2753891f518e9f34
Dominik Arapov <73648941+d-arapov@users.noreply.github.com>
2024-05-17 18:47:07 +0200

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -2,14 +2,15 @@ import { defaultWagmiConfig } from '@web3modal/wagmi/react/config'
 import { cookieStorage, createStorage } from 'wagmi'
 import { base } from 'wagmi/chains'
 
-export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
+// export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
+export const projectId = '6943b900e094b7dc40edf3cb3b14957e'
 
 if (!projectId) throw new Error('Project ID is not defined')
 
 const metadata = {
     name: 'Spin',
     description: 'Spin to win',
-    url: 'https://spin.com',
+    url: 'https://spin-frontend-bbb.vercel.app',
     icons: ['https://avatars.githubusercontent.com/u/37784886']
 }
 
